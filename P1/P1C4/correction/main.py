a2bf7a50bd5ba4606903cce25d80237f6c7eeace
Awgath <gawanda@thegasolutions.com>
2025-06-03 23:57:59 +0200

    Update main.py

--- P1/P1C4/correction/main.py
+++ P1/P1C4/correction/main.py
@@ -6,7 +6,7 @@ age = 25
 print(f"Je m'appelle {nom} et j'ai {age} ans.")
 
 # Modification de la variable age
-age = age + 10  #ou "age += 10"
+age = age + 10  #ou "age += 20"
 
 # Affichage de la phrase mise à jour avec les variables
 print(f"Je m'appelle {nom} et j'ai {age} ans maintenant.")
